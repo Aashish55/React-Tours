c33cb2126b33eed1f0fc29372a26e4755d5e153e
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-07-09 10:43:36 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CityTours from './Container/CityTours/CityTours'
+
+function App() {
+  return (
+    <div className="App">
+      <CityTours />
+    </div>
+  );
+}
+
+export default App;
